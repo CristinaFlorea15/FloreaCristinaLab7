7ef99da2860ea7ea4fd846f6b842e8dbd6d37ff2
CristinaFlorea15 <cristinafloreasb@gmail.com>
2024-12-01 18:23:59 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace FloreaCristinaLab7
+﻿using System;
+using FloreaCristinaLab7.Florea_Cristina_Lab10.Data;
+using System.IO;
+namespace FloreaCristinaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
